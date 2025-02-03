e283aa41095dfd14aa616d71991a40ccdff6bca8
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2025-02-03 18:03:30 -0300

    Add animation to the main character
    Walk and idle are available to the main character

--- player.gd
+++ player.gd
@@ -4,3 +4,9 @@ func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 600
 	move_and_slide()
+	
+	if velocity.length() > 0.0:
+		get_node("HappyBoo").play_walk_animation()
+	else:
+		get_node("HappyBoo").play_idle_animation()
+		
